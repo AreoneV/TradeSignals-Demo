0886bf5d88d5a8381aeed181a97b3e825a56bfa1
AreoneV <comp9224@gmail.com>
2023-11-30 22:59:35 +0300

    init: start testing protocol

---
+++ TestClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using Protocol;
+
+namespace TestClient;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Client client = new Client("127.0.0.1", 41222);
+
+
+
+        Console.ReadLine();
+
+        client.Connect();
+        Console.WriteLine("I'm connected!");
+        Console.ReadLine();
+
+        client.Close();
+
+        if (!client.IsConnected)
+        {
+            Console.WriteLine("I disconnected.");
+        }
+
+        Console.ReadLine();
+
+
+    }
+
+}
---
+++ TestServer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using Protocol;
+
+namespace TestServer;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Server server = new Server("127.0.0.1", 41222, 10);
+
+        server.ServerStarted += ServerOnServerStarted;
+        server.ServerStopped += ServerOnServerStopped;
+
+        server.UserConnected += ServerOnUserConnected;
+
+
+        server.Start(true);
+
+        Console.ReadLine();
+
+        server.Stop();
+        Console.ReadLine();
+    }
+
+    private static void ServerOnUserConnected(Server server, ClientConnected client)
+    {
+        Console.WriteLine("User connected.");
+        client.ClientDisconnected += ClientOnClientDisconnected;
+        client.ReceivedRequest += ClientOnReceivedRequest;
+    }
+
+    private static void ClientOnReceivedRequest(ClientConnected client, byte[] request, out byte[] answer)
+    {
+        Console.WriteLine($"Received request: {Encoding.UTF8.GetString(request)}");
+        answer = "Hi, client. I'm server!"u8.ToArray();
+    }
+
+    private static void ClientOnClientDisconnected(ClientConnected client)
+    {
+        Console.WriteLine("User disconnected.");
+    }
+
+    private static void ServerOnServerStopped(Server server)
+    {
+        Console.WriteLine("Server has stopped!");
+    }
+    private static void ServerOnServerStarted(Server server)
+    {
+        Console.WriteLine("Server has started!");
+    }
+}
