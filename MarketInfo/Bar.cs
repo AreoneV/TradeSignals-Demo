b6cb183966c9b73e314e983300f9871421765ee0
AreoneV <comp9224@gmail.com>
2023-12-01 08:37:19 +0300

    feat: add market info, bar and time frame

---
+++ MarketInfo/Bar.cs
@@ -0,0 +1,88 @@
+﻿namespace MarketInfo;
+
+
+/// <summary>
+/// Рыночный бар, содержит информацию о времени и о ценах открытия, закрытия, максимума и минимума
+/// </summary>
+public readonly struct Bar
+{
+    public Bar(DateTime date, float open, float close, float high, float low)
+    {
+        Date = date;
+        Open = open;
+        Close = close;
+        High = high;
+        Low = low;
+    }
+
+
+    public const int BarLength = 8 + 4 + 4 + 4 + 4;//дата + открытие, закрытие, максимум и минимум
+
+    /// <summary>
+    /// Дата
+    /// </summary>
+    public DateTime Date { get; }
+    /// <summary>
+    /// Цена открытия
+    /// </summary>
+    public float Open { get; }
+    /// <summary>
+    /// Цена закрытия
+    /// </summary>
+    public float Close { get; }
+    /// <summary>
+    /// Цена максимум
+    /// </summary>
+    public float High { get; }
+    /// <summary>
+    /// Цена минимума
+    /// </summary>
+    public float Low { get; }
+
+
+
+    
+    /// <summary>
+    /// Записывает себя в бинарное представление
+    /// </summary>
+    /// <param name="writer"></param>
+    public void Save(BinaryWriter writer)
+    {
+        writer.Write(Date.Ticks);
+        writer.Write(Open);
+        writer.Write(Close);
+        writer.Write(High);
+        writer.Write(Low);
+    }
+    /// <summary>
+    /// Создает новый бар из бинарного представления
+    /// </summary>
+    /// <param name="reader"></param>
+    /// <returns></returns>
+    public static Bar Create(BinaryReader reader)
+    {
+        return new Bar(new DateTime(reader.ReadInt64()), reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle(),
+            reader.ReadSingle());
+    }
+    /// <summary>
+    /// Создает массив баров из массива байтов
+    /// </summary>
+    /// <param name="data">Массив данных одного бара</param>
+    /// <returns></returns>
+    public static Bar[] Create(byte[] data)
+    {
+        using MemoryStream ms = new MemoryStream(data);
+        var reader = new BinaryReader(ms);
+        var arr = new Bar[data.Length / BarLength];
+        for (var i = 0; i < arr.Length; i++)
+        {
+            arr[i] = Create(reader);
+        }
+        return arr;
+    }
+
+    public override string ToString()
+    {
+        return $"{Date:HH:mm dd.MM.yyyy}";
+    }
+}
---
+++ MarketInfo/TimeFrame.cs
@@ -0,0 +1,46 @@
+﻿// ReSharper disable InconsistentNaming
+namespace MarketInfo;
+
+
+/// <summary>
+/// Временные рамки для построения графика
+/// </summary>
+public enum TimeFrame
+{
+    /// <summary>
+    /// 1 минута
+    /// </summary>
+    M1 = 1,  
+    /// <summary>
+    /// 5 минут
+    /// </summary>
+    M5 = 5,
+    /// <summary>
+    /// 15 минут
+    /// </summary>
+    M15 = 15,
+    /// <summary>
+    /// 30 минут
+    /// </summary>
+    M30 = 30,
+    /// <summary>
+    /// 1 час
+    /// </summary>
+    H1 = 60,
+    /// <summary>
+    /// 4 часа
+    /// </summary>
+    H4 = 240,
+    /// <summary>
+    /// 1 день
+    /// </summary>
+    D1 = 1440,
+    /// <summary>
+    /// 1 неделя
+    /// </summary>
+    W1 = 10080,
+    /// <summary>
+    /// 1 месяц
+    /// </summary>
+    MN1 = 43200
+}
