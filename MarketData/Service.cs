72f86917dae6c9cc0c10d3774d48989763758ffa
AreoneV <comp9224@gmail.com>
2023-12-02 17:01:31 +0300

    feat: add loading history and future

--- MarketData/Service.cs
+++ MarketData/Service.cs
@@ -16,6 +16,52 @@ public class Service
 
     private int maxLineLength = 1;
     
+    //Загрузка и добавление одного символа
+    private void Load(string name, int digits, string historyFile, string futureFile)
+    {
+        var fs = File.OpenRead(historyFile);
+        var br = new BinaryReader(fs);
+
+        Symbol symbol = new Symbol(name, digits);
+
+        var hst = symbol.Histories[TimeFrame.M1];
+
+        while (fs.Position < fs.Length)
+        {
+            hst.Add(Bar.Create(br));
+        }
+
+        foreach (var timeFrame in Enum.GetValues<TimeFrame>().OrderBy(x => x))
+        {
+            if(timeFrame == TimeFrame.M1) continue;
+
+            symbol.Histories[timeFrame].Add(hst.Bars);
+            hst = symbol.Histories[timeFrame];
+        }
+
+        symbols.Add(name, symbol);
+
+        br.Close();
+        fs.Dispose();
+
+        fs = File.OpenRead(futureFile);
+        br = new BinaryReader(fs);
+
+        var list = new List<Bar>((int)fs.Length / Bar.BarLength);
+
+        while(fs.Position < fs.Length)
+        {
+            list.Add(Bar.Create(br));
+        }
+
+        future.Add(name, list);
+
+        br.Close();
+        fs.Dispose();
+    }
+
+
+
     private void LogInfo(string msg)
     {
         var m = $"{DateTime.Now:G} | Info\t| {msg}";
