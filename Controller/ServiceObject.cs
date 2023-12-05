171b7a1e09b4656f07e0af3b5fae647279dd1474
AreoneV <comp9224@gmail.com>
2023-12-05 07:23:26 +0300

    feat: add service object for controling

---
+++ Controller/ServiceObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using Protocol;
+
+namespace Controller;
+
+public class ServiceObject
+{
+    
+    public string Name { get; set; }
+    public string FullPath { get; set; }
+    public int Id { get; set; }
+    public string Ip { get; set; }
+    public int Port { get; set; }
+
+    public Process Process { get; set; }
+    public Client Client { get; set; }
+
+
+
+
+    
+}
