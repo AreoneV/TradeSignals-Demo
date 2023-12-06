f52c53359764e8ca55f9eb366e8a1af5e5d07308
AreoneV <comp9224@gmail.com>
2023-12-06 23:05:39 +0300

    init: start testing controller

---
+++ TestService/Program.cs
@@ -0,0 +1,13 @@
+namespace TestService;
+
+internal static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        
+    }
+}
