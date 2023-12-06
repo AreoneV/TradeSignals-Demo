5ff98bc05196ff034aeb6cff991e63098b7d87b4
AreoneV <comp9224@gmail.com>
2023-12-06 07:42:42 +0300

    feat: add service management

---
+++ Controller/ServiceManagement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.ObjectModel;
+using Services;
+
+namespace Controller;
+
+public class ServiceManagement
+{
+    private const string SettingFile = "services.setting";
+
+
+    private Range portRange = new(41222, 41300);
+    private readonly Dictionary<ServiceNames, ServiceObject> services;
+
+
+    public ServiceManagement()
+    {
+        services = new Dictionary<ServiceNames, ServiceObject>();
+        Services = services.AsReadOnly();
+    }
+
+    public ReadOnlyDictionary<ServiceNames, ServiceObject> Services { get; }
+
+
+    public void Load()
+    {
+        ReadFile();
+    }
+    public void Start()
+    {
+
+    }
+    public void Stop()
+    {
+
+    }
+
+
+    private void ReadFile()
+    {
+        foreach(var value in Enum.GetValues<ServiceNames>())
+        {
+            services.Add(value, new ServiceObject(value, "127.0.0.1", Directory.GetCurrentDirectory() + $"\\{value}.exe"));
+        }
+        if(!File.Exists(SettingFile))
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Ok");
+            Console.ResetColor();
+            return;
+        }
+
+        using FileStream fs = new FileStream(SettingFile, FileMode.Open, FileAccess.Read);
+        BinaryReader br = new BinaryReader(fs);
+
+        try
+        {
+            while(fs.Position < fs.Length)
+            {
+                var name = (ServiceNames)br.ReadInt32();
+                var s = Services[name];
+                s.Ip = br.ReadString();
+                s.FullPath = br.ReadString();
+                s.AutoStart = br.ReadBoolean();
+            }
+        }
+        catch
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("...Warning");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Error loading setting file!");
+            Console.ResetColor();
+            return;
+        }
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Ok");
+        Console.ResetColor();
+    }
+}
