c3e4524587df772a795b06d6434b2fc262432fe7
AreoneV <comp9224@gmail.com>
2023-12-07 08:55:08 +0300

    feat: add starting and stopping service methods

--- Controller/ServiceObject.cs
+++ Controller/ServiceObject.cs
@@ -18,4 +18,80 @@ public class ServiceObject(ServiceNames name, string ip, string fullPath)
 
 
     
+    public void StartProcess(int port)
+    {
+        Port = port;
+
+        Process = Process.Start(FullPath, $"{Ip} {Port}");
+
+        if (Process == null || (Process.WaitForExit(1000) && Process.HasExited))
+        {
+            throw new Exception("Service didn't start. Error starting process");
+        }
+    }
+    public void StartConnect()
+    {
+        Client = new Client(Ip, Port);
+        Client.Connect();
+        Client.ClientDisconnected += ClientOnClientDisconnected;
+    }
+    public void StartListenInfo()
+    {
+        isStartedListen = true;
+        Task.Run(() =>
+        {
+            while (isStartedListen)
+            {
+                if (Process.HasExited)
+                {
+                    //if process closed
+                }
+
+
+                if(!Client.IsConnected)
+                {
+                    //stop and event
+                    break;
+                }
+
+                try
+                {
+                    var a = Client.Request(new byte[100], 1000);
+                }
+                catch(TimeoutException)
+                {
+                    //not responding
+                }
+                catch
+                {
+                    //close connection
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"  Process exit code          : {Process.ExitCode}");
+        });
+    }
+
+
+    public void Stop()
+    {
+        isStartedListen = false;
+        if (Client is { IsConnected: true })
+        {
+            Client.Close();
+            Client = null;
+        }
+
+        // ReSharper disable once InvertIf
+        if(Process is { HasExited: false})
+        {
+            Process.CloseMainWindow();
+            Process.Close();
+            Process = null;
+        }
+    }
+
+
+
 }
