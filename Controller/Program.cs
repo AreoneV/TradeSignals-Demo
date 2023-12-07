5a42398fd7ea1c9b9dff368fe00916dd196d8653
AreoneV <comp9224@gmail.com>
2023-12-07 08:31:54 +0300

    fix: change some commands

--- Controller/Program.cs
+++ Controller/Program.cs
@@ -74,32 +74,12 @@ internal class Program
                     Console.WriteLine();
 
                     Console.ForegroundColor = ConsoleColor.DarkCyan;
-                    Console.Write($"{empty}name");
-                    Console.ResetColor();
-                    Console.WriteLine(" - Show service names.");
-                    Console.WriteLine();
-
-                    Console.ForegroundColor = ConsoleColor.DarkCyan;
                     Console.Write($"{empty}log");
                     Console.ResetColor();
                     Console.ForegroundColor = ConsoleColor.DarkGreen;
                     Console.Write(" [count logs]");
                     Console.ResetColor();
                     Console.WriteLine(" - Show last logs. Example: log 20 - Show last 20 logs. If not enter number, show last 10 logs.");
-                    Console.WriteLine();
-
-                    Console.ForegroundColor = ConsoleColor.DarkCyan;
-                    Console.Write($"{empty}change ");
-                    Console.ResetColor();
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
-                    Console.Write(" [name] [property] [new value]");
-                    Console.ResetColor();
-                    Console.WriteLine(" - Changing service property.");
-                    Console.WriteLine($"{empty}Properties: Ip = '-n', Path = '-p', Auto Start = '-as'.");
-                    Console.WriteLine($"{empty}Path writes \"C:\\Folder\\File.exe\"; IP writes 127.0.0.1; Boolean writes true, false");
-                    Console.WriteLine($"{empty}Example: change MarketData -n 192.168.2.21 (change ip address);");
-                    Console.WriteLine($"{empty}Example: change AI -p \"C:\\MainFolder\\Folder2\\Service\\AI.exe (change path to executable file)\"");
-                    Console.WriteLine($"{empty}Example: change DB -as false (change auto start)");
                     Console.WriteLine();
 
                     Console.ForegroundColor = ConsoleColor.DarkCyan;
@@ -116,7 +96,7 @@ internal class Program
                     Console.ResetColor();
                     Console.WriteLine(" - Service management. Stopping, Starting and Information");
                     Console.WriteLine($"{empty}stop - stopping the service; start - starting the service");
-                    Console.WriteLine($"{empty}ping - check connection ping; info - resources information");
+                    Console.WriteLine($"{empty}ping - check connection ping");
                     Console.WriteLine();
 
                     Console.ForegroundColor = ConsoleColor.DarkCyan;
