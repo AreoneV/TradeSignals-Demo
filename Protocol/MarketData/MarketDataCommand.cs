ee79c574348f95b06f283d61b10c0ad116bf6930
AreoneV <comp9224@gmail.com>
2023-12-12 18:19:22 +0300

    docs: add comments for commands

--- Protocol/MarketData/MarketDataCommand.cs
+++ Protocol/MarketData/MarketDataCommand.cs
@@ -1,8 +1,20 @@
 ﻿namespace Protocol.MarketData;
 public enum MarketDataCommand
 {
+    /// <summary>
+    /// Запросить бары
+    /// </summary>
     GetBars,
+    /// <summary>
+    /// Запросить первую и последнюю даты
+    /// </summary>
     GetExtremeDate,
+    /// <summary>
+    /// Запросить последний бар
+    /// </summary>
     GetLasBar,
+    /// <summary>
+    /// Запросить имена всех символов
+    /// </summary>
     GetSymbolNames,
 }
