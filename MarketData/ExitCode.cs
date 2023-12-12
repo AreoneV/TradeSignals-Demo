dc44a5e3b672ac4af38a246b47e2f58e92624b82
AreoneV <comp9224@gmail.com>
2023-12-12 14:14:36 +0300

    feat: add exit codes

---
+++ MarketData/ExitCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketData;
+public enum ExitCode
+{
+    Ok = 0,
+    Killed,
+    InvalidArgs,
+    ErrorStarting
+}
