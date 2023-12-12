f4dc57f4cbb1a870d1e6ac0c39a4be17ee32a607
AreoneV <comp9224@gmail.com>
2023-12-12 12:08:06 +0300

    feat: add market data framework

---
+++ Protocol/MarketData/MarketDataFramework.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protocol.MarketData;
+public class MarketDataFramework(string ip, int port)
+{
+    private readonly Client client = new(ip, port);
+
+
+
+
+    
+}
