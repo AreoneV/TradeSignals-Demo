1581e0ad5e2d88963f3450f4b4aad2ff098a8a30
AreoneV <comp9224@gmail.com>
2023-12-13 06:19:19 +0300

    feat: add initialization weigths for NN

---
+++ AI/Initialization/IInitialization.cs
@@ -0,0 +1,10 @@
+﻿namespace AI.Initialization;
+public interface IInitialization
+{
+    /// <summary>
+    /// Производит инициализацию весов и смещений
+    /// </summary>
+    /// <param name="b">Смещения которые нужно инициализировать</param>
+    /// <param name="w">Веса которые нужно инициализировать</param>
+    void Initialize(float[] b, float[,] w);
+}
---
+++ AI/Initialization/RandomWeights.cs
@@ -0,0 +1,44 @@
+﻿namespace AI.Initialization;
+
+/// <summary>
+/// Инициализирует веса и смещения рандомными значениями в диапазоне
+/// </summary>
+public class RandomWeights : IInitialization
+{
+    private readonly Random r = new();
+    private readonly float min = -0.1f;
+    private readonly float max = 0.1f;
+    public RandomWeights()
+    {
+
+    }
+    public RandomWeights(float max, float min)
+    {
+        this.min = min;
+        this.max = max;
+    }
+
+    /// <summary>
+    /// Производит инициализацию весов и смещений
+    /// </summary>
+    /// <param name="b">Смещения которые нужно инициализировать</param>
+    /// <param name="w">Веса которые нужно инициализировать</param>
+    public void Initialize(float[] b, float[,] w)
+    {
+        //вычисляем дельту
+        var p = max - min;
+        for(int i = 0; i < b.Length; i++)
+        {
+            //вычисляем рандом в диапазоне и присваеваем смещению
+            b[i] = min + (float)r.NextDouble() * p;
+        }
+        for(int i = 0; i < w.GetLength(0); i++)
+        {
+            for(int j = 0; j < w.GetLength(1); j++)
+            {
+                //вычисляем рандом в диапазоне и присваеваем весу
+                w[i, j] = min + (float)r.NextDouble() * p;
+            }
+        }
+    }
+}
