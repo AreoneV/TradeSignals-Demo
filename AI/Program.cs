520a379b92f879324d6954061f6195c9e82229d2
AreoneV <comp9224@gmail.com>
2023-12-13 05:15:48 +0300

    init: start working on AI project

---
+++ AI/Program.cs
@@ -0,0 +1,14 @@
+namespace AI;
+
+internal static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    internal static int Main(string[] args)
+    {
+
+        return 0;
+    }
+}
