46f18cdb729046cc1418451e86bcca93f6b70057
AreoneV <comp9224@gmail.com>
2023-12-15 23:15:45 +0300

    feat: add symbol object

---
+++ AI/Symbol.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using MarketInfo;
+
+namespace AI;
+
+public class Symbol
+{
+    private readonly Dictionary<TimeFrame, BasicNetwork> buyNetworks = [];
+    private readonly Dictionary<TimeFrame, BasicNetwork> sellNetworks = [];
+
+    public Symbol(string name)
+    {
+        Name = name;
+        Load();
+        BuyNetworks = buyNetworks.AsReadOnly();
+        SellNetworks = sellNetworks.AsReadOnly();
+    }
+
+
+    public string Name { get; }
+    public ReadOnlyDictionary<TimeFrame, BasicNetwork> BuyNetworks { get; }
+    public ReadOnlyDictionary<TimeFrame, BasicNetwork> SellNetworks { get; }
+
+
+
+    private void Load()
+    {
+
+    }
+}
