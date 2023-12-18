4c7a8ad558b014c470f2eaecea2945c695268bb7
AreoneV <comp9224@gmail.com>
2023-12-18 07:37:32 +0300

    init: start work with data base service

---
+++ DataBase/Program.cs
@@ -0,0 +1,14 @@
+namespace DataBase;
+
+internal static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static int Main(string[] args)
+    {
+
+        return 0;
+    }
+}
